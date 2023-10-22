b9bd28bbca781fde1dd2394b4a2f9db315e4e353
Kashif Shahzad <mkashifshahzad125@gmail.com>
2023-10-22 08:26:25 -0700

    Calculator final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      display: "",
+    };
+  }
+
+  addToDisplay = (value) => {
+    this.setState((prevState) => ({
+      display: prevState.display + value,
+    }));
+  };
+
+  clearDisplay = () => {
+    this.setState({
+      display: "",
+    });
+  };
+
+  calculateResult = () => {
+    try {
+      this.setState({
+        display: eval(this.state.display) || "0",
+      });
+    } catch (error) {
+      this.setState({
+        display: "Error",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div class="conainer">
+        <div class="body">
+          <h2 class="heading">PUNJAB COLLEGE</h2>
+          <input
+            className="LCD numbers"
+            type="text"
+            id="display"
+            value={this.state.display}
+            readOnly
+          />
+          <div class="grid">
+            <div class="btn__holder ce">
+              <button class="btn" onClick={this.clearDisplay}>
+                CE
+              </button>
+            </div>
+            <div class="btn__holder ce">
+              <button class="btn" onClick={this.clearDisplay}>
+                CE
+              </button>
+            </div>
+            <div class="btn__holder ce">
+              <button class="btn" onClick={this.clearDisplay}>
+                %
+              </button>
+            </div>
+            <div class="btn__holder ce">
+              <button class="btn" onClick={() => this.addToDisplay("/")}>
+                ÷
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("1")}>
+                1
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("2")}>
+                2
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("3")}>
+                3
+              </button>
+            </div>
+            <div class="btn__holder ce">
+              <button class="btn" onClick={() => this.addToDisplay("*")}>
+                x
+              </button>
+            </div>
+
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("4")}>
+                4
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("5")}>
+                5
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("6")}>
+                6
+              </button>
+            </div>
+            <div class="btn__holder dvd">
+              <button className="btn" onClick={() => this.addToDisplay("-")}>
+                -
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("7")}>
+                7
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("8")}>
+                8
+              </button>
+            </div>
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("9")}>
+                9
+              </button>
+            </div>
+            <div class="btn__holder ce">
+              <button class="btn" onClick={() => this.addToDisplay("+")}>
+                +
+              </button>{" "}
+            </div>
+
+            <div class="btn__holder">
+              <button className="btn" onClick={() => this.addToDisplay("0")}>
+                0
+              </button>
+            </div>
+            <div class="btn__holder point">
+              <button class="btn" onClick={() => this.addToDisplay(".")}>
+                .
+              </button>
+            </div>
+
+            <div class="btn__holder btn__long ">
+              <button class="btn" onClick={this.calculateResult}>
+                =
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
